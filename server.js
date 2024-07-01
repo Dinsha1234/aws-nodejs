12c2e6e3b664b99e02abc2d52aa967ad54fd216e
Dinsha1234 <dinsha.hemachandran12@gmail.com>
2024-07-01 14:00:12 +0530

    retrieving user details information

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ app.get('/get/user', (req,res)=>{
     res.send({message: "hello back to node js"})
 })
 
+app.get('/get/user_details', (req,res)=>{
+    res.send({
+        user: {
+            name: "Dinsha",
+            age: 25,
+            contact:12234
+        }
+    })
+})
+
 app.listen(3000,()=> {
     console.log('listening to port', 3000)
 })
